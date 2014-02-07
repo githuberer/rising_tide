de4179d5fe0fa8befd933fda4e443df9fbd00dc6
yy <yy@yy.com>
2014-02-07 17:41:46 +0800

    NULL

--- init.sh
+++ init.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+
+. /usr/local/rvm/scripts/rvm
+
 param=$1
 
 case $param in
     start)
-        nohup ./app.rb &>> /var/log/rising_tide.log &
+        echo -n "Start Rising_tide ... "
+        nohup /usr/bin/env ruby /home/wyy/rising_tide/app.rb &> /var/log/rising_tide.log &
+        echo $! > /run/rising_tide.pid
+        echo "pid: $(cat /run/rising_tide.pid)"
         ;;
     stop)
         kill -9 $(ps -ef |grep app.rb |grep -v "vim" |grep -v "grep" |awk '{print $2}' |paste -s -d " ")
@@ -15,6 +21,5 @@ case $param in
     echo -e "\n PARAMS: start | stop | status \n"
     ;;
 esac
-
 
 
