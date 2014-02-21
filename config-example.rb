6cc81075f5ad344eaf478cddfe0e93796a8f4f20
yy <yy@yy.com>
2014-02-21 18:01:52 +0800

    null

---
+++ config-example.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# This is a example file of config.rb
+# Modify variables below and then execute shell command: 'mv config-example.rb config.rb'
+
+# ssh :user, :port, :password
+$port_ssh = '22'
+$user_ssh = 'user1'
+$password_ssh = "password1"
+
+# hosts
+$hosts = {
+  "v5app" => "192.168.1.1",
+  "v5app2" => "192.168.1.2",
+  "v5file" => "192.168.1.3",
+  "v5backup" => "192.168.1.4",
+  "v5db2" =>  "192.168.1.5", 
+  "v5db" => "192.168.1.6"
+}
+
+#sync mysql: maichang original music
+$user_mysql = 'user2'
+$password_mysql = 'password2'
+$database = 'test'
+$table = 'test'
+$fields_filepath = %w{ om_path lyric_url karaoke_url intonation_url lrc }
+
+
